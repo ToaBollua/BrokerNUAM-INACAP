ad2f84366dc3fec677621a2c60829301b19a073c
alumnos nunoa <alumnosnunoa@inacap.cl>
2025-11-05 10:40:36 -0300

    Fix: Bifurcation & Refactor

--- brokernuam/brokernuam/settings.py
+++ brokernuam/brokernuam/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# --- Added by ChatGPT fix ---
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
